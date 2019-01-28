71c9a39637b4eb8afb78e30e200bf20991a54ed1
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-01-28 18:13:45 -0500

    Added all siteContent to index.html, working on styling + task 4

--- js/index.js
+++ js/index.js
@@ -44,6 +44,20 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 // Task 2a: Create DOM selectors
 let links = Array.from(document.querySelectorAll('a'));
 
+let ctaTitle = document.querySelector('.cta-text h1');
+
+let ctaButton = document.querySelector('.cta-text button');
+
+let topContentTitles = Array.from(document.querySelectorAll('.text-content h4'));
+
+let topContentParag = Array.from(document.querySelectorAll('.text-content p'));
+
+let contactTitle = document.querySelector('.contact h4');
+
+let contactParag = Array.from(document.querySelectorAll('.contact p'));
+
+let copyright = document.querySelector('footer p')
+
 // Task 2b: Update image src's
 let ctaImg = document.getElementById("cta-img");
 ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
@@ -51,7 +65,58 @@ ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
 let middleImg = document.getElementById("middle-img");
 middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
 
+// Below doesn't work? From lecture
+// let middleImg = document.querySelector("#middle-img");
+// middleImg.src(siteContent["main-content"]["middle-img-src"]);
+
 //Task 3: Update the HTML with JSON data
 for (let i=0; i < 6; i++) {
   links[i].textContent = siteContent['nav'][`nav-item-${i+1}`]
-}
+};
+
+ctaTitle.textContent = siteContent['cta']['h1'];
+
+console.log(ctaTitle.textContent)
+
+ctaButton.textContent = siteContent['cta']['button'];
+
+for (let i=0; i < topContentTitles.length; i++) { 
+  if (i === 0) {
+    topContentTitles[i].textContent = siteContent['main-content'][`features-h4`];
+  } else if (i === 1) {
+    topContentTitles[i].textContent = siteContent['main-content'][`about-h4`];
+  } else if (i === 2) {
+    topContentTitles[i].textContent = siteContent['main-content'][`services-h4`];
+  } else if (i === 3) {
+    topContentTitles[i].textContent = siteContent['main-content'][`product-h4`];
+  } else {
+    topContentTitles[i].textContent = siteContent['main-content'][`vision-h4`];
+  }
+};
+
+for (let i=0; i < topContentParag.length; i++) { 
+  if (i === 0) {
+    topContentParag[i].textContent = siteContent['main-content'][`features-content`];
+  } else if (i === 1) {
+    topContentParag[i].textContent = siteContent['main-content'][`about-content`];
+  } else if (i === 2) {
+    topContentParag[i].textContent = siteContent['main-content'][`services-content`];
+  } else if (i === 3) {
+    topContentParag[i].textContent = siteContent['main-content'][`product-content`];
+  } else {
+    topContentParag[i].textContent = siteContent['main-content'][`vision-content`];
+  }
+};
+
+contactTitle.textContent = siteContent['contact']['contact-h4']
+
+for (let i = 1; i<Object.keys(siteContent['contact']).length; i++) {
+  contactParag[i-1].textContent = siteContent['contact'][Object.keys(siteContent['contact'])[i]];
+};
+
+copyright.textContent = siteContent['footer']['copyright']
+
+
+
+
+
